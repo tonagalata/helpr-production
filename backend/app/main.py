df38d92d6c20aacb3a73bc6df17e36a58e07cd46
James Eubanks <jeubanks.github@gmail.com>
2022-07-05 23:44:38 -0400

    allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://172.16.231.98:8080"],
+    allow_origins=["http://172.16.231.98:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
